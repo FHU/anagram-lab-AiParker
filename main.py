a699e19138c9acb7695b5c142b546f20b4abc0bd
AiParker <jordan.parker@students.fhu.edu>
2024-02-10 18:07:22 -0600

    First Draft
    Added code

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(str1, str2):
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+    
+    return sorted(str1) == sorted(str2)
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    input1 = input("Enter the first string: ")
+    input2 = input("Enter the second string: ")
     
+    result = anagram(input1, input2)
+    print(result)
+
